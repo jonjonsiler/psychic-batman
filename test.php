feb32e2f6cf9fd0ddc0f4be9e4d2cc8d81cdc57a
Jonathan Siler <jonjonsiler@users.noreply.github.com>
2015-08-25 23:13:05 -0500

    Updating data and pushing to server to merge

---
+++ test.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+    <title>This is the title.</title>
+    </head>
+    <body><p>
+<?php 
+$v = "original items";
+echo $v;
+
+$i ="another item";
+echo $i;
+
+#this is the last item
+$p = "Another item";
+echo $p;
+?>
+    We'll add some additional PHP code here just to see how difficult it is to manage the merge from git
+        <?php 
+    for($i = 1; $i<=20; $i++){
+        echo $i;
+    }
+        ?>
+</body>
+</html>
