3e39a616c3d8ff4199f4e93cdd75b26740cb2fe8
Jonathan Siler <jsiler@istation.com>
2022-02-18 08:31:28 -0600

    Commit from a working branch

--- test.php
+++ test.php
@@ -13,12 +13,18 @@ echo $i;
 #this is the last item
 $p = "Another item";
 echo $p;
+
+// Guess what heres some php that i did in a working branch
+$are_you_sure = true;
+
 ?>
     We'll add some additional PHP code here just to see how difficult it is to manage the merge from git
         <?php
+if ($are_you_sure):
     for($i = 1; $i<=20; $i++){
         echo $i;
     }
+endif;
         ?>
 </body>
 </html>
